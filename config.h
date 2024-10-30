f744a99778344c3ae36b9d2ef6f21e7da32a04d3
Do Khanh Nguyen <162601401+nguynvjpro@users.noreply.github.com>
2024-10-30 21:55:53 +0700

    Create config.h

---
+++ config.h
@@ -0,0 +1,10 @@
+#define ssid      "ESP32"             //AP SSID
+#define pass      ""                //AP Password
+
+String  Essid = "";                 //EEPROM Network SSID
+String  Epass = "";                 //EEPROM Network Password
+String  Etoken = "";                //EEPROM Network token
+
+String  sssid = "";                 //Read SSID From Web Page
+String  passs = "";                 //Read Password From Web Page
+String  token = "";                 //Read token From Web Page
